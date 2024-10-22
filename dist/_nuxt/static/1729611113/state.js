3e4a85c062c553e29e853440f4a288b59719888b
Jared0103 <josejared8ac@hotmail.com>
2024-10-22 16:48:17 -0600

    revisamos errores

--- dist/_nuxt/static/1729611113/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1729611113",layout:"login",error:a,state:{token:a,auth:{user:a,loggedIn:false,strategy:"local"}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
