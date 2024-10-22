d9d3b7c2dc98919dcde89a4e383015392451b3ad
Jared0103 <josejared8ac@hotmail.com>
2024-10-22 17:38:53 -0600

    espero ya funcione

---
+++ dist/_nuxt/e9336f5.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{529:function(t,n,e){"use strict";e.r(n);var o={layout:"dashboard"},r=e(68),component=Object(r.a)(o,(function(){this._self._c;return this._m(0)}),[function(){var t=this._self._c;return t("div",[t("h1",[this._v("dashboard")])])}],!1,null,"b687ae44",null);n.default=component.exports}}]);
